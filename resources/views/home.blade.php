83c0534f11f2790e5e36653e16c94ce5fce12a3b
Vincent Kyle Caballa <vincentkyle27@gmail.com>
2021-10-09 00:13:52 +0800

    changes

---
+++ resources/views/home.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
+    <title>Home | JuanBarangay</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="nav-sidebar">
+                    <ul class="nav flex-column">
+                        <h6 class="pt-3 pl-3">DASHBOARD</h6>
+                        <li class="nav-item">
+                            <div class="col-md-12">
+                                <a class="align-items-center nav-link active" href="#"><img class="ml-3 mr-3 p-2"
+                                        src="assets/icons8-home.png" alt="">Home</a>
+                            </div>
+                        </li>
+
+                        <li class="nav-item">
+                            <div class="col-md-12">
+                                <a class="nav-link" href="#"><img class="ml-3 mr-3 p-2"
+                                        src="assets/icons8-manager.png" alt="">Barangay Officials</a>
+                            </div>
+
+                        </li>
+
+                        <li class="nav-item">
+                            <div class="col-md-12">
+                                <a class="nav-link" href="#"> <img class="ml-2 mr-3 p-2"
+                                        src="assets/icons8-people.png" alt="">Barangay Population</a>
+                            </div>
+                        </li>
+
+                        <li class="nav-item">
+                            <div class="col-md-12">
+                                <a class="nav-link" href="#"><img class="ml-3 mr-3 p-2" src="assets/icons8-calendar.png"
+                                        alt="">Calendar of Activities</a>
+                            </div>
+                        </li>
+
+                        <h6 class="pt-3 pl-3">CERTIFICATES</h6>
+                        <li class="nav-item">
+                            <a class="nav-link h-20" href="">Barangay Forms</a>
+                        </li>
+
+                        <h6 class="pt-3 pl-3">INFORMATION HUB</h6>
+                        <li class="nav-item">
+                            <a class="nav-link h-20" href="">About the Team</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-9">
+
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('login');
 });
+
+Route::get('/', function () {
+    return view('home');
+});
