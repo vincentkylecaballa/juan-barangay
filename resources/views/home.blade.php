452a9e932add914e8f74f66a496be0ae750f109b
Vincent Kyle Caballa <vincentkyle27@gmail.com>
2021-10-13 21:55:55 +0800

    changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,10 +7,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/home.css') }}" rel="stylesheet">
     <link href="/css/app.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <title>Home | JuanBarangay</title>
 </head>
 
 <body>
+
+
+
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-3">
@@ -75,11 +81,28 @@
                 </div>
             </div>
             <div class="col-lg-9">
-
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-9">
+                            <h1 class="pt-5">Home</h1>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="dropdown pt-5">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Dropdown button
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
-    </div>
 </body>
 
 </html>
